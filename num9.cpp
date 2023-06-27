feaa72204374b3387a81a6e5c8f346da186722c2
Jinyeong <121329066+Jinyeong-7369@users.noreply.github.com>
2023-06-28 08:32:51 +0900

    Update num9.cpp

--- num9.cpp
+++ num9.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    bool isPalindrome(int x);
-};
-bool Solution::isPalindrome(int x)
-{
-    string s;
-    int tmp;
-    int n;
-    if(x < 0) return false;
-    else if(x < 10) return true;
-    else
-    {
-        s = to_string(x);
-        n = s.length();
-        for(int i = 0; i < n/2; i++)
+    bool isPalindrome(int x) {
+        // 1. 한자리 수면 무조건 충족
+        // 2. 음수면 무조건 아님
+        // 자리수 별로 나눈다
+        // 끝 자리 첫 자리 비교 하기
+        int dq[10];
+        int front = 0;
+        int back = -1;
+        if(x < 0) return false;
+        else if(x < 10) return true;
+        else
         {
-            if(s[i] != s[n - i - 1]) return false;
+            while(x != 0)
+            {
+                back++;
+                dq[back] = x % 10;
+                x /= 10;
+            }
+            while(front < back)
+            {  
+                if(dq[front++] != dq[back--]) return false;
+            }
         }
         return true;
     }
-}
+};
