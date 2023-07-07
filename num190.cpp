e604d86676b1cd823cb494bd265ca54282127c39
unknown <yjy7369@gmail.com>
2023-07-07 16:51:59 +0900

    modify num190

--- num190.cpp
+++ num190.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        stack<bool> s;
-        int bit_add = 32;
-        while(n)
-        {
-            bit_add--;
-            s.push(n % 2);
-            n /= 2;
-        }
+        uint32_t k = 2147483648;
         uint32_t result = 0;
-        uint32_t k = 1;
-        while(!s.empty())
+        while (n)
         {
-            result += k * (uint32_t)s.top();
-            s.pop();
-            k *= 2;
-        }
-        for(int i = 0; i < bit_add; i++)
-        {
-            result*=2;
+            result += (n % 2) * k;
+            n /= 2;
+            k /= 2;
         }
         return result;
-
     }
 };
