71dad7335f5e059d582a84238701c13c3771d97d
unknown <yjy7369@gmail.com>
2023-07-11 18:07:54 +0900

    add problem num 101(Balanced Binary Tree)

---
+++ num110.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if (root == NULL) return true;
+        else
+        {
+            int gapOfSideTree = getHeight(root->right) - getHeight(root->left);
+            if (gapOfSideTree > 1) return false;
+            else if (gapOfSideTree < -1) return false;
+            else return isBalanced(root->right) && isBalanced(root->left);
+        }
+    }
+    int getHeight(TreeNode* root)
+    {
+        if (root != NULL) return 1 + max(getHeight(root->left), getHeight(root->right));
+        else return 0;
+    }
+};
