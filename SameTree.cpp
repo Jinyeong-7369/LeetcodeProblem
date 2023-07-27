7744d0e8ecdc6d1b0c9de5675effdcd385da1f06
Jinyeong7369 <yjy7369@gmail.com>
2023-07-27 13:07:46 +0900

    solved sametree using recursion

---
+++ SameTree.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL) return true;
+        else if(p != NULL && q != NULL && p->val == q->val) return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        else return false;
+    }
+};
